1fad0200d618b095b6910c23cffc57d3dda33e8d
RaeHoon Baek <bmp117@naver.com>
2022-07-25 19:58:11 +0900

    LEDBoard 중간 커밋!

---
+++ LEDBoard/SettingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingViewController.swift
+//  LEDBoard
+//
+//  Created by 백래훈 on 2022/07/25.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
